655e071de4650097a7c763381bf200787cd9f0e2
Fabio Gebhard <fabiogebhard@gmail.com>
2021-05-27 00:48:34 +0200

    added DenseNetSeg network

--- src/main.py
+++ src/main.py
@@ -1,46 +1,63 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
 from tqdm.auto import tqdm
 from utils.dataloader import AneurysmDataset
+from utils.DenseSeg import DenseNetSeg3D
+
+
 
 
 if __name__ == "__main__":
 
-    data_path = ...  # insert absolute path to the data directory
+    data_path = 'src/preprocessed'  # insert absolute path to the data directory
     target_resolution = (64, 64, 64)  # modify here if other resolution needed
     batch_size = 6
 
     train = torch.utils.data.DataLoader(
-            AneurysmDataset(
-                data_path = data_path,
-                target_resolution = target_resolution,
-                train = True
-            ),
-            batch_size=batch_size,
-            shuffle=True
-        )
+        AneurysmDataset(
+            data_path=data_path,
+            target_resolution=target_resolution,
+            train=True
+        ),
+        batch_size=batch_size,
+        shuffle=True
+    )
 
     test = torch.utils.data.DataLoader(
-            AneurysmDataset(
-                data_path = data_path,
-                target_resolution = target_resolution,
-                train = False
-            ),
-            batch_size=batch_size,
-            shuffle=True
-        )
+        AneurysmDataset(
+            data_path=data_path,
+            target_resolution=target_resolution,
+            train=False
+        ),
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    model = DenseNetSeg3D()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     for epoch in tqdm(range(20), desc='Epoch'):
 
         # training
         for train_step, [train_ex, train_l] in enumerate(tqdm(train, desc='Train')):
+            # forward
+            scores = model(train_ex)
+            scores = torch.squeeze(scores)
+            loss = criterion(scores, train_l.type(torch.LongTensor))
 
+            # backward
+            optimizer.zero_grad()
+            loss.backward()
+
+            optimizer.step()
             # TODO: train network
             print(train_ex.shape)
             print(train_l.shape)
 
         # test
         for test_step, [test_ex, test_l] in enumerate(tqdm(test, desc='Test')):
-
             # TODO: test network
             print(test_ex.shape)
             print(test_l.shape)
---
+++ src/utils/DenseSeg.py
@@ -0,0 +1,178 @@
+import torch
+import torch.nn as nn
+
+
+class DenseNetSeg3D(nn.Module):
+    # growth_rate in our case 16
+    def __init__(self):
+        super(DenseNetSeg3D, self).__init__()
+
+        self.batch_norm = nn.BatchNorm3d(num_features=32)
+        self.relu = nn.ReLU()
+
+        self.conv1 = nn.Conv3d(1, 32, kernel_size=(1, 1, 1), stride=(1, 1, 1))
+        self.conv2 = nn.Conv3d(32, 32, kernel_size=(1, 1, 1), stride=(1, 1, 1))
+        self.conv3 = nn.Conv3d(32, 32, kernel_size=(1, 1, 1), stride=(1, 1, 1))
+
+        self.conv_down = nn.Conv3d(32, 32, kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 0, 0))
+        self.sigmoid = nn.Sigmoid()
+
+        self.growth_rate = 16
+        self.reduction = 0.5
+        self.in_channels = 32
+
+    def forward(self, x):
+        # 3x initial Convolution, after this layers output should be 64x64x64 and 32 channels
+        x = self.conv1(x)
+        x = self.batch_norm(x)
+        x = self.relu(x)
+
+        x = self.conv2(x)
+        x = self.batch_norm(x)
+        x = self.relu(x)
+
+        x1 = self.conv3(x)
+        x = self.batch_norm(x1)
+        x = self.relu(x)
+
+        # first convolution for Downsampling
+        x = self.conv_down(x)
+
+        # DenseBlock 1
+        x = DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate)(x)
+        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+
+        # Deconvolution 1
+        x2 = Deconvolution(in_channels=self.in_channels, kernel_size=(4, 4, 4), stride=(2, 2, 2))(x)
+
+        # Transition 1 -> includes Downsampling
+        x = TransitionLayer(in_channels=self.in_channels, reduction=self.reduction)(x)
+        self.in_channels = int(self.in_channels * self.reduction)
+
+        # DenseBlock 2
+        x = DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate)(x)
+        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+
+        # Deconvolution 2
+        x3 = Deconvolution(in_channels=self.in_channels, kernel_size=(6, 6, 6), stride=(4, 4, 4))(x)
+
+        # Transition 2 -> includes Downsampling
+        x = TransitionLayer(in_channels=self.in_channels, reduction=self.reduction)(x)
+        self.in_channels = int(self.in_channels * self.reduction)
+
+        # DenseBlock 3
+        x = DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate)(x)
+        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+
+        # Deconvolution 3
+        x4 = Deconvolution(in_channels=self.in_channels, kernel_size=(10,10,10), stride=(8,8,8))(x)
+
+        # Transition 3 -> includes Downsampling
+        x = TransitionLayer(in_channels=self.in_channels, reduction=self.reduction)(x)
+        self.in_channels = int(self.in_channels * self.reduction)
+
+        # DenseBlock 4
+        x = DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate)(x)
+        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+
+        x = nn.BatchNorm3d(self.in_channels)(x)
+        x = self.relu(x)
+
+        # Deconvolution 4
+        x5 = Deconvolution(in_channels=self.in_channels, kernel_size=(18, 18, 18), stride=(16, 16, 16))(x)
+
+        # concatenation of upscaled
+        x = torch.cat([x5, x4, x3, x2, x1], dim=1)
+
+        x = nn.Conv3d(96, out_channels=2, kernel_size=(1, 1, 1), stride=(1, 1, 1))(x)
+        x = self.sigmoid(x)
+        #import numpy
+        #z = x.detach().numpy()
+        return x
+
+
+class DenseBlock(nn.Module):
+    '''
+    in_channel in this case = growth rate?!
+    '''
+
+    def __init__(self, in_channels, n_layers, growth_rate):
+        super(DenseBlock, self).__init__()
+        self.n_layers = n_layers
+        self.in_channels = in_channels
+        self.growth_rate = growth_rate
+
+    def forward(self, x):
+        for i in range(self.n_layers):
+            # increases with each layer e.g. start with 64 -> 96 -> 128..
+            in_channels = int(i * self.growth_rate + self.in_channels)
+
+            # in_channels are out_channels from the last layer
+            y = DenseLayer(in_channels, self.growth_rate)(x)
+            x = torch.cat([x, y], dim=1)
+        return x
+
+
+class DenseLayer(nn.Module):
+    '''
+    All Convolution Layers in this networks belongs to this BN-ReLU-Conv implementation
+    '''
+
+    def __init__(self, in_channels, growth_rate):
+        super(DenseLayer, self).__init__()
+        self.batch_norm = nn.BatchNorm3d(in_channels)
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv3d(in_channels, in_channels, kernel_size=(1, 1, 1), stride=(1, 1, 1))
+        self.conv2 = nn.Conv3d(in_channels, growth_rate, kernel_size=(3, 3, 3), stride=(1, 1, 1),
+                               padding=(1, 1, 1))
+        self.drop_out = nn.Dropout(p=0.2)
+
+    def forward(self, x):
+        # first conv(1x1x1)
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        x = self.conv1(x)
+
+        # second conv(3x3x3)
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+
+        # after each Conv (3x3x3) dropout layer
+        x = self.drop_out(x)
+
+        return x
+
+
+class TransitionLayer(nn.Module):
+    def __init__(self, in_channels, reduction):
+        super(TransitionLayer, self).__init__()
+        self.batch_norm = nn.BatchNorm3d(in_channels)
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv3d(in_channels, in_channels, kernel_size=(1, 1, 1), stride=(1, 1, 1))
+        self.conv2 = nn.Conv3d(in_channels, int(in_channels * reduction), kernel_size=(2, 2, 2), stride=(2, 2, 2))
+
+    def forward(self, x):
+        # first conv(1x1x1)
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        x = self.conv1(x)
+
+        # second downscaling conv(3x3x3)
+        x = self.batch_norm(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+
+        return x
+
+
+class Deconvolution(nn.Module):
+    def __init__(self, in_channels, kernel_size, stride):
+        super(Deconvolution, self).__init__()
+        self.deconv1 = nn.ConvTranspose3d(in_channels, 16, kernel_size=kernel_size, stride=stride,
+                                          padding=(1, 1, 1))
+
+    def forward(self, x):
+        x = self.deconv1(x)
+
+        return x
