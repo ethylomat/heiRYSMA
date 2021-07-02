dba1b6ac140ddfaf879316a2cba020c10477c41c
Lennart Stipulkowski <stipulkowski@stud.uni-heidelberg.de>
2021-07-02 15:38:55 +0200

    Minor changes

--- etc/queue.py
+++ etc/queue.py
@@ -13,7 +13,7 @@ def submit(batch_size=8, overlap=30, resolution=64, learning_rate="0.001", wall_
         os.mkdir(os.path.join("log", job_name))
     except:
         pass
-    output = f"log/{job_name}/%J_info.txt"
+    output = f"log/{job_name}/%J_output.txt"
     error = f"log/{job_name}/%J_error.txt"
 
     gpu_reservation = ""
@@ -22,7 +22,7 @@ def submit(batch_size=8, overlap=30, resolution=64, learning_rate="0.001", wall_
     if "gpu_4" in partition:
         gpu_reservation = " --gres=gpu:4"
 
-    command = f'sbatch -d singleton --export={DICE}BATCH_SIZE={batch_size},OVERLAP={overlap},RESOLUTION="{resolution} {resolution} {resolution}",LEARNING_RATE={learning_rate},ID="{job_name}",LOSS={loss} --job-name="{job_name}" --partition={partition}{gpu_reservation} -t {wall_time} --output={output} --error={error} submit.sh'
+    command = f'sbatch -d singleton --export=BATCH_SIZE={batch_size},OVERLAP={overlap},RESOLUTION="{resolution} {resolution} {resolution}",LEARNING_RATE={learning_rate},ID="{job_name}",LOSS={loss} --job-name="{job_name}" --partition={partition}{gpu_reservation} -t {wall_time} --output={output} --error={error} submit.sh'
     print("Running command: ", command)
     os.system(command)
     time.sleep(1)
