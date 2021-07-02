d008a5c469208d608bba1ea1a73d707275f7e01a
MarinkoBa <64740488+MarinkoBa@users.noreply.github.com>
2021-07-02 13:20:18 +0200

    rapair DenseNet weight update, add dice & focal loss

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from tqdm.auto import tqdm
+import os
 from utils.dataloader import AneurysmDataset
 from utils.DenseSeg import DenseNetSeg3D
 import numpy as np
+from utils.dice_loss import BinaryDiceLoss
+from utils.focal_loss import FocalLoss
 
 
 def run_model_get_scores(example, label, device, target_resolution, sum_aneurysm_truth_batch, sum_aneurysm_pred_batch, loss_batch, file, epoch, step, train=True):
@@ -13,7 +16,7 @@ def run_model_get_scores(example, label, device, target_resolution, sum_aneurysm
     example = example.to(device)
     example = example.double()
 
-    scores = model(example, device, target_resolution)
+    scores = model(example, target_resolution)
     scores = torch.squeeze(scores)
     loss = criterion(scores, label.float())
     loss_batch.append(loss.item())
@@ -38,12 +41,26 @@ def run_model_get_scores(example, label, device, target_resolution, sum_aneurysm
 
     return sum_aneurysm_truth_batch, sum_aneurysm_pred_batch, loss_batch
 
-def create_loss_log_file(model_name):
+def create_loss_log_file(loss_log_file_name):
 
-    f = open('log/' + model_name + '_loss_log.txt', 'a')
-    f.write('Log file start for the test: ' + model_name + '_loss_log.txt\n')
+    f = open('log/' + loss_log_file_name + '_loss_log.txt', 'a')
+    f.write('Log file start for the test: ' + loss_log_file_name + '_loss_log.txt\n')
 
     return f
+
+def create_current_best_loss_file(best_loss_file_name):
+
+    if (os.path.isfile('log/' +  best_loss_file_name + '_best_loss_log.txt')):
+            f = open('log/' +  best_loss_file_name + '_best_loss_log.txt', "r+")
+            lines = f.read().splitlines()
+            best_loss = lines[-1]
+            best_loss = float(best_loss)
+
+    else:
+        f = open('log/' + best_loss_file_name + '_best_loss_log.txt', 'w')
+        best_loss = 100.0
+
+    return f, best_loss
 
 def write_stats_after_epoch(sum_aneurysm_truth_batch, sum_aneurysm_pred_batch, loss_batch, epoch, train_eval, file):
     print(train_eval + ', epoch: ' + str(epoch))
@@ -61,13 +78,14 @@ if __name__ == "__main__":
 
     data_path = ...  # insert absolute path to the data directory
     target_resolution = (64, 64, 64)  # modify here if other resolution needed, currently available (64, 64, 64) and (128, 128, 100)
-    overlap = 10  # overlap for cropping
-    batch_size = 8
+    overlap = 20  # overlap for cropping
+    batch_size = 6
     include_augmented_data = False  # enable if flipped data (vertically + horizonatally), rotated data (180 degrees) and brighter data 5% wanted
     include_resizing = False  # enable if resizing wanted, else cropping applied
-    model_name = 'model_64_64_64_10_8_0001_crop'  # resolution_overlap_batchsize_learning_rate
+    model_name = 'model_64_64_64_20_8_0001_crop_Focal_Loss_sigmoid'  # resolution_overlap_batchsize_learning_rate
 
-    file = create_loss_log_file(model_name)
+    loss_log_file = create_loss_log_file(model_name)
+    best_loss_log_file, curr_best_batch_loss = create_current_best_loss_file(model_name)
 
     train = torch.utils.data.DataLoader(
         AneurysmDataset(
@@ -97,27 +115,31 @@ if __name__ == "__main__":
         drop_last=True
     )
 
-    test = torch.utils.data.DataLoader(
-        AneurysmDataset(
-            data_path=data_path,
-            target_resolution=target_resolution,
-            overlap=overlap,
-            include_augmented_data=include_augmented_data,
-            include_resizing=include_resizing,
-            train_eval_test='test'
-        ),
-        batch_size=batch_size,
-        shuffle=True,
-        drop_last=True
-    )
+    # test = torch.utils.data.DataLoader(
+    #     AneurysmDataset(
+    #         data_path=data_path,
+    #         target_resolution=target_resolution,
+    #         overlap=overlap,
+    #         include_augmented_data=include_augmented_data,
+    #         include_resizing=include_resizing,
+    #         train_eval_test='test'
+    #     ),
+    #     batch_size=batch_size,
+    #     shuffle=True,
+    #     drop_last=True
+    # )
 
     device = torch.device(0 if torch.cuda.is_available() else "cpu")
 
-    model = DenseNetSeg3D()
-    model.to(device)
+    model = DenseNetSeg3D(device=device)
     model = model.double()
-    model = torch.nn.parallel.DataParallel(model)  # -> enable for parallelism, device ids are gpus for calculation
-    criterion = nn.BCEWithLogitsLoss()
+    model.to(device)
+
+    model.load_state_dict(torch.load(model_name))
+
+    #criterion = nn.BCEWithLogitsLoss()
+    #criterion = BinaryDiceLoss()
+    criterion = FocalLoss(weight=torch.tensor(np.array([0.25])), gamma=2, reduction='mean')  # check if 0.25 or 0.75???
     criterion.to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.0001)
 
@@ -130,12 +152,12 @@ if __name__ == "__main__":
         for train_step, [train_ex, train_l] in enumerate(tqdm(train, desc='Train')):
 
             sum_aneurysm_truth_batch_train, sum_aneurysm_pred_batch_train, loss_batch_train = run_model_get_scores(train_ex, train_l, device, target_resolution,
-                                                                                                                    sum_aneurysm_truth_batch_train, sum_aneurysm_pred_batch_train, loss_batch_train,
-                                                                                                                    file, epoch, train_step, train=True)
+                                                                                                                   sum_aneurysm_truth_batch_train, sum_aneurysm_pred_batch_train, loss_batch_train,
+                                                                                                                   loss_log_file, epoch, train_step, train=True)
 
-        write_stats_after_epoch(sum_aneurysm_truth_batch_train, sum_aneurysm_pred_batch_train, loss_batch_train, epoch, 'Train', file)
+        write_stats_after_epoch(sum_aneurysm_truth_batch_train, sum_aneurysm_pred_batch_train, loss_batch_train, epoch, 'Train', loss_log_file)
 
-        if (epoch + 1) % 10 == 0:
+        if (epoch + 1) % 1 == 0:
 
             sum_aneurysm_truth_batch_eval = 0
             sum_aneurysm_pred_batch_eval = 0
@@ -145,11 +167,18 @@ if __name__ == "__main__":
             for eval_step, [eval_ex, eval_l] in enumerate(tqdm(eval, desc='Eval')):
                 sum_aneurysm_truth_batch_eval, sum_aneurysm_pred_batch_eval, loss_batch_eval = run_model_get_scores(eval_ex, eval_l, device, target_resolution,
                                                                                                                     sum_aneurysm_truth_batch_eval, sum_aneurysm_pred_batch_eval, loss_batch_eval,
-                                                                                                                    file, epoch, eval_step, train=False)
+                                                                                                                    loss_log_file, epoch, eval_step, train=False)
 
-            torch.save(model.state_dict(), model_name)
+            if (np.mean(loss_batch_eval) < curr_best_batch_loss):
+                print("Current best batch loss: " + str(curr_best_batch_loss))
+                print("New best batch loss: " + str(np.mean(loss_batch_eval)))
+                print("Store model...")
+                curr_best_batch_loss = np.mean(loss_batch_eval)
+                torch.save(model.state_dict(), model_name)
+                best_loss_log_file.write(str(curr_best_batch_loss) + '\n')
+                best_loss_log_file.flush()
 
             write_stats_after_epoch(sum_aneurysm_truth_batch_eval, sum_aneurysm_pred_batch_eval, loss_batch_eval,
-                                    epoch, 'Eval', file)
+                                    epoch, 'Eval', loss_log_file)
 
-    file.close()
+    loss_log_file.close()
--- src/utils/DenseSeg.py
+++ src/utils/DenseSeg.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class DenseNetSeg3D(nn.Module):
     # growth_rate in our case 16
-    def __init__(self):
+    def __init__(self, device):
         super(DenseNetSeg3D, self).__init__()
 
         self.batch_norm = nn.BatchNorm3d(num_features=32)
@@ -16,13 +16,33 @@ class DenseNetSeg3D(nn.Module):
         self.conv3 = nn.Conv3d(32, 32, kernel_size=(1, 1, 1), stride=(1, 1, 1))
 
         self.conv_down = nn.Conv3d(32, 32, kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 0, 0))
-        self.sigmoid = nn.Sigmoid()
 
         self.growth_rate = 16
         self.reduction = 0.5
-        self.in_channels = 32
 
-    def forward(self, x, device, target_resolution):
+        self.dense_block_1 = DenseBlock(in_channels=32, n_layers=4, growth_rate=self.growth_rate, device=device)
+        self.deconvolution1 = torch.nn.DataParallel(Deconvolution(in_channels=96, kernel_size=(4, 4, 4), stride=(2, 2, 2))).to(device)
+        self.transition1 = torch.nn.DataParallel(TransitionLayer(in_channels=96, reduction=self.reduction)).to(device)
+
+        self.dense_block_2 = DenseBlock(in_channels=48, n_layers=4, growth_rate=self.growth_rate, device=device)
+        self.deconvolution2 = torch.nn.DataParallel(Deconvolution(in_channels=112, kernel_size=(6, 6, 6), stride=(4, 4, 4))).to(device)
+        self.transition2 = torch.nn.DataParallel(TransitionLayer(in_channels=112, reduction=self.reduction)).to(device)
+
+        self.dense_block_3 = DenseBlock(in_channels=56, n_layers=4, growth_rate=self.growth_rate, device=device)
+        self.deconvolution3_128 = torch.nn.DataParallel(Deconvolution(in_channels=120, kernel_size=(10, 10, 14), stride=(8, 8, 8))).to(device)
+        self.deconvolution3_64 = torch.nn.DataParallel(Deconvolution(in_channels=120, kernel_size=(10,10,10), stride=(8,8,8))).to(device)
+        self.transition3 = torch.nn.DataParallel(TransitionLayer(in_channels=120, reduction=self.reduction)).to(device)
+
+        self.dense_block_4 = DenseBlock(in_channels=60, n_layers=4, growth_rate=self.growth_rate, device=device)
+        self.batch_norm2_3D = torch.nn.DataParallel(nn.BatchNorm3d(124)).to(device)
+        self.deconvolution4_128 = torch.nn.DataParallel(Deconvolution(in_channels=124, kernel_size=(18, 18, 22), stride=(16, 16, 16))).to(device)
+        self.deconvolution4_64 = torch.nn.DataParallel(Deconvolution(in_channels=124, kernel_size=(18, 18, 18), stride=(16, 16, 16))).to(device)
+        self.conv4 = torch.nn.DataParallel(nn.Conv3d(96, out_channels=1, kernel_size=(1, 1, 1), stride=(1, 1, 1))).to(device)
+
+        self.sigmoid = nn.Sigmoid()
+
+
+    def forward(self, x, target_resolution):
         x = torch.unsqueeze(x, 1)
 
         # 3x initial Convolution, after this layers output should be 64x64x64 and 32 channels
@@ -42,62 +62,55 @@ class DenseNetSeg3D(nn.Module):
         x = self.conv_down(x)
 
         # DenseBlock 1
-        x = torch.nn.parallel.DataParallel(DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate).double())(x, device)
-        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+        x = self.dense_block_1(x)
 
         # Deconvolution 1
-        x2 = torch.nn.parallel.DataParallel(Deconvolution(in_channels=self.in_channels, kernel_size=(4, 4, 4), stride=(2, 2, 2)).double().to(device))(x)
+        x2 = self.deconvolution1(x)
 
         # Transition 1 -> includes Downsampling
-        x = torch.nn.parallel.DataParallel(TransitionLayer(in_channels=self.in_channels, reduction=self.reduction).double().to(device))(x)
-        self.in_channels = int(self.in_channels * self.reduction)
+        x = self.transition1(x)
 
         # DenseBlock 2
-        x = torch.nn.parallel.DataParallel(DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate).double())(x, device)
-        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+        x = self.dense_block_2(x)
 
         # Deconvolution 2
-        x3 = torch.nn.parallel.DataParallel(Deconvolution(in_channels=self.in_channels, kernel_size=(6, 6, 6), stride=(4, 4, 4)).double().to(device))(x)
+        x3 = self.deconvolution2(x)
 
         # Transition 2 -> includes Downsampling
-        x = torch.nn.parallel.DataParallel(TransitionLayer(in_channels=self.in_channels, reduction=self.reduction).double().to(device))(x)
-        self.in_channels = int(self.in_channels * self.reduction)
+        x = self.transition2(x)
 
         # DenseBlock 3
-        x = torch.nn.parallel.DataParallel(DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate).double())(x, device)
-        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+        x = self.dense_block_3(x)
 
         # Deconvolution 3
         if target_resolution == (128,128,100):
-            x4 = torch.nn.parallel.DataParallel(Deconvolution(in_channels=self.in_channels, kernel_size=(10, 10, 14), stride=(8, 8, 8)).double().to(device))(x)
+            x4 = self.deconvolution3_128(x)
         else:
             # 64x64x64 is default!
-            x4 = torch.nn.parallel.DataParallel(Deconvolution(in_channels=self.in_channels, kernel_size=(10,10,10), stride=(8,8,8)).double().to(device))(x)
+            x4 = self.deconvolution3_64(x)
 
         # Transition 3 -> includes Downsampling
-        x = torch.nn.parallel.DataParallel(TransitionLayer(in_channels=self.in_channels, reduction=self.reduction).double().to(device))(x)
-        self.in_channels = int(self.in_channels * self.reduction)
+        x = self.transition3(x)
 
         # DenseBlock 4
-        x = torch.nn.parallel.DataParallel(DenseBlock(in_channels=self.in_channels, n_layers=4, growth_rate=self.growth_rate).double())(x, device)
-        self.in_channels = int(self.in_channels + self.growth_rate * 4)
+        x = self.dense_block_4(x)
 
-        x = nn.BatchNorm3d(self.in_channels).double().to(device)(x)
+        x = self.batch_norm2_3D(x)
         x = self.relu(x)
 
         # Deconvolution 4
         if target_resolution == (128,128,100):
-            x5 = torch.nn.parallel.DataParallel(Deconvolution(in_channels=self.in_channels, kernel_size=(18, 18, 22), stride=(16, 16, 16)).double().to(device))(x)
+            x5 = self.deconvolution4_128(x)
         else:
             #64x64x64 is default!
-            x5 = torch.nn.parallel.DataParallel(Deconvolution(in_channels=self.in_channels, kernel_size=(18, 18, 18), stride=(16, 16, 16)).double().to(device))(x)
+            x5 = self.deconvolution4_64(x)
 
         # concatenation of upscaled
-        x = torch.cat([x5, x4, x3, x2, x1.to(device)], dim=1)
+        x = torch.cat([x5, x4, x3, x2, x1], dim=1)
 
-        x = nn.Conv3d(96, out_channels=1, kernel_size=(1, 1, 1), stride=(1, 1, 1)).double().to(device)(x)
+        x = self.conv4(x)
 
-        self.in_channels = 32
+        x = self.sigmoid(x)
         return x
 
 
@@ -106,20 +119,27 @@ class DenseBlock(nn.Module):
     in_channel in this case = growth rate?!
     '''
 
-    def __init__(self, in_channels, n_layers, growth_rate):
+    def __init__(self, in_channels, n_layers, growth_rate, device):
         super(DenseBlock, self).__init__()
         self.n_layers = n_layers
         self.in_channels = in_channels
         self.growth_rate = growth_rate
+        self.dense_layers = []
 
-    def forward(self, x, device):
+        for i in range(self.n_layers):
+            model = DenseLayer(int(i * self.growth_rate + self.in_channels), self.growth_rate)
+            model = model.double()
+            dense_layer = torch.nn.DataParallel(model)
+            dense_layer.to(device)
+            self.dense_layers.append(dense_layer)
+
+    def forward(self, x):
         for i in range(self.n_layers):
             # increases with each layer e.g. start with 64 -> 96 -> 128..
-            in_channels = int(i * self.growth_rate + self.in_channels)
 
             # in_channels are out_channels from the last layer
-            y = torch.nn.parallel.DataParallel(DenseLayer(in_channels, self.growth_rate).to(device)).double()(x)
-            x = torch.cat([x.to(device), y], dim=1)
+            y = self.dense_layers[i](x)
+            x = torch.cat([x, y], dim=1)
         return x
 
 
@@ -181,6 +201,7 @@ class Deconvolution(nn.Module):
         super(Deconvolution, self).__init__()
         self.deconv1 = nn.ConvTranspose3d(in_channels, 16, kernel_size=kernel_size, stride=stride,
                                           padding=(1, 1, 1))
+        self.deconv1 = self.deconv1
 
     def forward(self, x):
         x = self.deconv1(x)
