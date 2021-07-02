902480988298d897dcafb6b540f930cd54f3606a
Lennart Stipulkowski <stipulkowski@stud.uni-heidelberg.de>
2021-07-02 14:44:11 +0200

    Update: Select partition in queue.py script

--- etc/queue.py
+++ etc/queue.py
@@ -4,10 +4,10 @@ import os, time
 prefix = ""
 
 def main():
-    for i in range(4):
+    for i in range(1):
         submit(learning_rate= "0.0001", overlap=30)
 
-def submit(batch_size=8, overlap=30, resolution=64, learning_rate="0.001", wall_time="01:59:00", gpu=8, loss="DIC"):
+def submit(batch_size=8, overlap=30, resolution=64, learning_rate="0.001", wall_time="01:59:00", loss="DIC", partition="gpu_8"):
     job_name = f"{prefix}o{overlap}lr{str(learning_rate).replace('0.', '')}_{loss}"
     try:
         os.mkdir(os.path.join("log", job_name))
@@ -16,7 +16,13 @@ def submit(batch_size=8, overlap=30, resolution=64, learning_rate="0.001", wall_
     output = f"log/{job_name}/%J_info.txt"
     error = f"log/{job_name}/%J_error.txt"
 
-    command = f'sbatch -d singleton --export={DICE}BATCH_SIZE={batch_size},OVERLAP={overlap},RESOLUTION="{resolution} {resolution} {resolution}",LEARNING_RATE={learning_rate},ID="{job_name}",LOSS={loss} --job-name="{job_name}" --partition=gpu_{gpu} --gres=gpu:{gpu} -t {wall_time} --output={output} --error={error} submit.sh'
+    gpu_reservation = ""
+    if "gpu_8" in partition:
+        gpu_reservation = " --gres=gpu:8"
+    if "gpu_4" in partition:
+        gpu_reservation = " --gres=gpu:4"
+
+    command = f'sbatch -d singleton --export={DICE}BATCH_SIZE={batch_size},OVERLAP={overlap},RESOLUTION="{resolution} {resolution} {resolution}",LEARNING_RATE={learning_rate},ID="{job_name}",LOSS={loss} --job-name="{job_name}" --partition={partition}{gpu_reservation} -t {wall_time} --output={output} --error={error} submit.sh'
     print("Running command: ", command)
     os.system(command)
     time.sleep(1)
